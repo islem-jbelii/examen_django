5d3068a1f8b58df7f4b352a04859041a7ad1c47d
islem127 <islem.jbeli@sesame.com.tn>
2026-05-30 14:17:15 +0100

    Fix: add missing youth_platform.wsgi

---
+++ youth_platform/wsgi.py
@@ -0,0 +1,11 @@
+"""WSGI config for youth_platform project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youth_platform.settings')
+
+application = get_wsgi_application()
